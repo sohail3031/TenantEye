f978986e7db945bf5cef3003b4c9df856e4ef697
sohail3031 <mohdsohailahmed46@gmail.com>
2023-11-25 19:42:48 -0500

    ForgotPasswordSelectionActivity.java is completed

--- app/src/main/java/com/example/tenanteye/forgotpassword/ForgotPasswordMobileActivity.java
+++ app/src/main/java/com/example/tenanteye/forgotpassword/ForgotPasswordMobileActivity.java
@@ -11,22 +11,28 @@ import com.example.tenanteye.R;
 import com.example.tenanteye.resetpassword.ResetPasswordMobileActivity;
 
 public class ForgotPasswordMobileActivity extends AppCompatActivity {
+    AppCompatButton verifyCodeButton;
+    private ImageView backImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password_mobile);
 
-        ImageView backImage = findViewById(R.id.forgot_password_mobile_back_image_button);
-        AppCompatButton verifyCode = findViewById(R.id.forgot_password_mobile_button);
+        initializeAllVariables();
 
         backImage.setOnClickListener(view -> {
             finish();
         });
 
-        verifyCode.setOnClickListener(view -> {
+        verifyCodeButton.setOnClickListener(view -> {
             startActivity(new Intent(this, ResetPasswordMobileActivity.class));
             finish();
         });
+    }
+
+    private void initializeAllVariables() {
+        backImage = findViewById(R.id.forgot_password_mobile_back_image_button);
+        verifyCodeButton = findViewById(R.id.forgot_password_mobile_button);
     }
 }
--- app/src/main/java/com/example/tenanteye/forgotpassword/ForgotPasswordSelectionActivity.java
+++ app/src/main/java/com/example/tenanteye/forgotpassword/ForgotPasswordSelectionActivity.java
@@ -1,29 +1,46 @@
 package com.example.tenanteye.forgotpassword;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tenanteye.R;
+import com.example.tenanteye.login.LoginActivity;
 import com.example.tenanteye.resetpassword.ResetPasswordEmailActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class ForgotPasswordSelectionActivity extends AppCompatActivity {
+    private ImageView backImage;
+    private Button mobileButton, emailButton;
+    private TextView mobileTextView, emailTextView;
+    private Bundle bundle;
+    private String emailAddress, phoneNumber;
+    private FirebaseAuth firebaseAuth;
+
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password_selection);
 
-        ImageView backImage = findViewById(R.id.forgot_password_selection_back_image_view);
-        Button mobileButton = findViewById(R.id.forgot_password_selection_mobile_button);
-        Button emailButton = findViewById(R.id.forgot_password_selection_email_button);
+        initializeAllVariables();
+        getUserDataFromBundle();
+        showUserDataOnUI();
 
-//        backImage.setOnClickListener(view -> {
-//            finish();
-//        });
+        backImage.setOnClickListener(view -> {
+            showAlertMessage();
+        });
 
         mobileButton.setOnClickListener(view -> {
             startActivity(new Intent(this, ForgotPasswordMobileActivity.class));
@@ -31,8 +48,60 @@ public class ForgotPasswordSelectionActivity extends AppCompatActivity {
         });
 
         emailButton.setOnClickListener(view -> {
-            startActivity(new Intent(this, ResetPasswordEmailActivity.class));
-            finish();
+            sendResetPasswordEmail();
         });
+    }
+
+    private void sendResetPasswordEmail() {
+        firebaseAuth.sendPasswordResetEmail(emailAddress).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(ForgotPasswordSelectionActivity.this, ResetPasswordEmailActivity.class));
+                } else {
+                    Toast.makeText(ForgotPasswordSelectionActivity.this, "" + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void showUserDataOnUI() {
+        mobileTextView.setText(phoneNumber);
+        emailTextView.setText(emailAddress);
+    }
+
+    private void showAlertMessage() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder
+                .setTitle(R.string.sign_up_alert_title)
+                .setMessage(R.string.sign_up_alert_message)
+                .setPositiveButton(R.string.alert_yes, (dialog, which) -> {
+                    startActivity(new Intent(this, LoginActivity.class));
+                    finish();
+                })
+                .setNegativeButton(R.string.alert_no, (dialog, which) -> {
+                    dialog.dismiss();
+                });
+
+        AlertDialog alertDialog = builder.create();
+
+        alertDialog.show();
+    }
+
+    private void getUserDataFromBundle() {
+        emailAddress = bundle.getString("emailAddress");
+        phoneNumber = bundle.getString("phoneNumber");
+    }
+
+    private void initializeAllVariables() {
+        backImage = findViewById(R.id.forgot_password_selection_back_image_view);
+        mobileButton = findViewById(R.id.forgot_password_selection_mobile_button);
+        emailButton = findViewById(R.id.forgot_password_selection_email_button);
+        mobileTextView = findViewById(R.id.forgot_password_selection_mobile_text_view_info);
+        emailTextView = findViewById(R.id.forgot_password_selection_email_text_view_info);
+
+        bundle = getIntent().getExtras();
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 }
